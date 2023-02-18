adf2cbef5c0cc8bffa4d4eec89308317c8a2abc8
Enric Pina <epina87@gmail.com>
2023-02-18 08:46:47 +0100

    website tags y instalacion de mongoose

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -13,14 +11,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Variables Globales
+app.locals.title = 'NodePop'
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.locals.tags = ['work',' lifestyle', 'motor','mobile'];
+
+  res.render('index');
 });
 
+
+// GET /tags 
+router.get('/tags', function(req, res, next) {
+  res.locals.tags = ['work',' lifestyle', 'motor','mobile'];
+
+  res.render('tags');
+});
+
+
+
 module.exports = router;
+
+
